c8b806750aa519987d823864d68089eef9227bc0
ngdloc2000 <ngdinhloc2000@gmail.com>
2023-05-15 14:36:03 +0700

    cm

--- src/app/component/management/construction/construction-profile/construction-profile.component.ts
+++ src/app/component/management/construction/construction-profile/construction-profile.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-construction-profile',
   templateUrl: './construction-profile.component.html',
-  styleUrls: ['./construction-profile.component.css', '../../main/main.component.css']
+  styleUrls: ['./construction-profile.component.css']
 })
-export class ConstructionProfileComponent {
+export class ConstructionProfileComponent implements OnInit {
+
+  selectedDiv: number = 0;
+
+  constructor(private elementRef: ElementRef,
+              private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  isSelected(profileId: number) {
+    return this.selectedDiv === profileId;
+  }
+
+  tabProfileSelected(profileId: number) {
+    const element = this.elementRef.nativeElement.querySelector('#profile' + profileId);
+    this.selectedDiv = profileId;
+    this.renderer.addClass(element, 'active');
+  }
 
 }
